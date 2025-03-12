b48a1f79f327cc641f07d13e734a3a589ea819a9
alishamis <alishm1@uci.edu>
2025-03-12 03:49:10 +0000

    fixed to use dp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,5 +1,6 @@
 #include "ladder.h"
-
+#include <algorithm>
+#include <unordered_set>
 void error(string word1, string word2, string msg) {
     cerr << "Error between words: " << word1 << " and " << word2 << ". " << msg << endl;
 }
@@ -35,39 +36,47 @@ bool is_adjacent(const string& word1, const string& word2) {
     return edit_count + abs(len1 - i) + abs(len2 - j) == 1;
 }
 
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+ vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (begin_word == end_word) {
-        return {begin_word};
+        return {begin_word};  // Should return a list with only the word itself
     }
 
     queue<vector<string>> ladder_queue;
-    set<string> visited;
-    
+    unordered_set<string> visited;
+    unordered_set<string> level_visited;
+
     ladder_queue.push({begin_word});
     visited.insert(begin_word);
 
     while (!ladder_queue.empty()) {
-        vector<string> ladder = ladder_queue.front();
-        ladder_queue.pop();
-        string last_word = ladder.back();
+        int size = ladder_queue.size();
+        level_visited.clear();
 
-        for (const auto& word : word_list) {
-            if (is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
-                visited.insert(word);
-                vector<string> new_ladder = ladder;
-                new_ladder.push_back(word);
+        for (int i = 0; i < size; i++) {
+            vector<string> ladder = ladder_queue.front();
+            ladder_queue.pop();
+            string last_word = ladder.back();
 
-                if (word == end_word) {
-                    return new_ladder;
+            for (const auto& word : word_list) {
+                if (is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    
+                    if (word == end_word) return new_ladder;
+                    
+                    ladder_queue.push(new_ladder);
+                    level_visited.insert(word);
                 }
-                
-                ladder_queue.push(new_ladder);
             }
         }
+        
+        // Update visited set after finishing the level
+        visited.insert(level_visited.begin(), level_visited.end());
     }
 
     return {};
 }
+
 
 void load_words(set<string>& word_list, const string& file_name) {
     ifstream file(file_name);
@@ -86,9 +95,9 @@ void load_words(set<string>& word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
-        cout << "No ladder found!" << endl;
+        cout << "No word ladder found." << endl;  // Fix wording
     } else {
-        cout << "Word ladder: ";
+        cout << "Word ladder found: ";
         for (const auto& word : ladder) {
             cout << word << " ";
         }
@@ -96,38 +105,32 @@ void print_word_ladder(const vector<string>& ladder) {
     }
 }
 
+
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     int len1 = str1.length();
     int len2 = str2.length();
     
-    if (abs(len1 - len2) > d)
-        return false;
-    
-    int e = 0;
-    int i = 0, j = 0;
-    
-    while (i < len1 && j < len2) {
-        if (str1[i] != str2[j]) {
-            e++;
-            if (e > d)
-                return false;
-        }
-        
-        if (len1 > len2) {
-            i++;
-        } else if (len1 < len2) {
-            j++;
-        } else {
-            i++;
-            j++;
+    if (abs(len1 - len2) > d) return false;
+
+    int dp[len1 + 1][len2 + 1];
+
+    for (int i = 0; i <= len1; i++) {
+        for (int j = 0; j <= len2; j++) {
+            if (i == 0)
+                dp[i][j] = j;  // Insertions
+            else if (j == 0)
+                dp[i][j] = i;  // Deletions
+            else if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];  // Match
+            else
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});  // Edit
         }
     }
     
-    e += abs(len1 - i);
-    e += abs(len2 - j);
-    
-    return e <= d;
+    return dp[len1][len2] <= d;
 }
+  
+
 
 
 void verify_word_ladder() {
